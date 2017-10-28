572081f553c1b2a6359b9ab03ab2cfe6a39c24bd
chiayutsaipersonal <chiayu.tsai.personal@gmail.com>
2017-10-28 11:23:25 +0800

    some reference video or website about things on the todo list

--- src/server/controllers/emails/emails.js
+++ src/server/controllers/emails/emails.js
@@ -2,6 +2,9 @@ import cron from 'node-cron'
 
 import jobScheduler from './jobScheduler'
 
+// how to use nodemailer with gmail
+// https://www.youtube.com/watch?v=JJ44WA_eV8E
+
 // how often to broadcast messages
 // const BROADCAST_FREQUENCY = '0 * * * * *'
 // const BROADCAST_FREQUENCY = '*/30 * * * * *'
@@ -11,15 +14,15 @@ const BROADCAST_FREQUENCY = '*/15 * * * * *'
 // const BROADCAST_FREQUENCY = '* * * * * *'
 
 class EmailBroadcastSystem {
-    constructor(broadcastFrequency) {
-        this.job = cron.schedule(broadcastFrequency, jobScheduler, false)
-    }
-    initialize() {
-        return new Promise((resolve, reject) => {
-            this.job.start()
-            resolve('Email broadcast system initialized...')
-        })
-    }
+  constructor (broadcastFrequency) {
+    this.job = cron.schedule(broadcastFrequency, jobScheduler, false)
+  }
+  initialize () {
+    return new Promise((resolve, reject) => {
+      this.job.start()
+      resolve('Email broadcast system initialized...')
+    })
+  }
 }
 
 const emailBroadcastSystem = new EmailBroadcastSystem(BROADCAST_FREQUENCY)
