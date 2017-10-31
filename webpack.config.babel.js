c7cebba132e8a0024973e289d444bdce31963036
chiayutsaipersonal <chiayu.tsai.personal@gmail.com>
2017-10-31 21:01:41 +0800

    webpack fixed path listings

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -6,90 +6,90 @@ import webpack from 'webpack'
 
 import HtmlWebpackPlugin from 'html-webpack-plugin'
 
-import devServer from './src/workflow/webpack.config/devServer'
-import eslint from './src/workflow/webpack.config/eslint'
-import extractCss from './src/workflow/webpack.config/extractCss'
-import purifyCss from './src/workflow/webpack.config/purifyCss'
-import sourceMaps from './src/workflow/webpack.config/sourceMaps'
-import style from './src/workflow/webpack.config/style'
-import transpiler from './src/workflow/webpack.config/transpilers'
+import devServer from './src/workflow/webpack/devServer'
+import eslint from './src/workflow/webpack/eslint'
+import extractCss from './src/workflow/webpack/extractCss'
+import purifyCss from './src/workflow/webpack/purifyCss'
+import sourceMaps from './src/workflow/webpack/sourceMaps'
+import style from './src/workflow/webpack/style'
+import transpiler from './src/workflow/webpack/transpilers'
 
 dotEnv.config()
 const eVars = process.env
 
 let PATHS = {
-    app: path.resolve('./src/client'),
-    assets: path.resolve('./src/client/assets'),
-    build: path.resolve('./dist/public')
+  app: path.resolve('./src/client'),
+  assets: path.resolve('./src/client/assets'),
+  build: path.resolve('./dist/public')
 }
 
 const commonConfig = merge([
-    {
-        entry: {
-            app: ['babel-polyfill', PATHS.app]
-        },
-        output: {
-            path: PATHS.build,
-            filename: '[name].js'
-        },
-        plugins: [
-            new HtmlWebpackPlugin({
-                title: eVars.SYS_REF,
-                filename: path.join(PATHS.build, 'index.html'),
-                favicon: path.join(PATHS.assets, 'favicon.ico'),
-                template: path.join(PATHS.assets, 'index.html'),
-                hash: true,
-                showErrors: eVars.NODE_ENV === 'production',
-                inject: 'body'
-            })
-        ]
+  {
+    entry: {
+      app: ['babel-polyfill', PATHS.app]
     },
-    transpiler.babel({ include: PATHS.app }),
-    eslint({ include: PATHS.app })
+    output: {
+      path: PATHS.build,
+      filename: '[name].js'
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: eVars.SYS_REF,
+        filename: path.join(PATHS.build, 'index.html'),
+        favicon: path.join(PATHS.assets, 'images/logos/favicon.ico'),
+        template: path.join(PATHS.assets, 'index.html'),
+        hash: true,
+        showErrors: eVars.NODE_ENV === 'production',
+        inject: 'body'
+      })
+    ]
+  },
+  transpiler.babel({ include: PATHS.app }),
+  eslint({ include: PATHS.app })
 ])
 
 const productionConfig = merge([
-    {
-        entry: {
-            vendor: ['react']
-        },
-        plugins: [
-            new webpack.optimize.CommonsChunkPlugin({
-                name: 'vendor'
-            })
-        ]
+  {
+    entry: {
+      vendor: ['react']
     },
-    sourceMaps({ type: 'source-map' }),
-    extractCss({
-        use: [
-            'css-loader',
-            'sass-loader',
-            style.autoprefix()
-        ]
-    }),
-    purifyCss({
-        paths: glob.sync(`${PATHS.app}/**/*.js`, { nodir: true })
-    })
+    plugins: [
+      new webpack.optimize.CommonsChunkPlugin({
+        name: 'vendor'
+      })
+    ]
+  },
+  sourceMaps({ type: 'source-map' }),
+  extractCss({
+    use: [
+      'css-loader',
+      'sass-loader',
+      style.autoprefix()
+    ]
+  }),
+  purifyCss({
+    paths: glob.sync(`${PATHS.app}/**/*.js`, { nodir: true })
+  })
 ])
 
 const developmentConfig = merge([
-    {
-        output: {
-            devtoolModuleFilenameTemplate: 'webpack:///[absolute-resource-path]'
-        }
-    },
-    sourceMaps({ type: 'cheap-module-eval-source-map' }),
-    devServer({
-        host: eVars.DEV_HOST,
-        port: eVars.PORT
-    }),
-    style.loadCss(),
-    style.loadSass()
+  {
+    output: {
+      devtoolModuleFilenameTemplate: 'webpack:///[absolute-resource-path]'
+    }
+  },
+  sourceMaps({ type: 'cheap-module-eval-source-map' }),
+  devServer({
+    host: eVars.DEV_HOST,
+    port: eVars.PORT
+  }),
+  style.loadCss(),
+  style.loadSass()
 ])
 
 module.exports = (env) => {
-    if (env === 'production') {
-        return merge(commonConfig, productionConfig)
-    }
-    return merge(commonConfig, developmentConfig)
+  if (env === 'production') {
+    return merge(commonConfig, productionConfig)
+  }
+  return merge(commonConfig, developmentConfig)
 }
