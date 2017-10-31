e0187f92f222cb5cfda6c74a1fed97f44f804b1e
chiayutsaipersonal <chiayu.tsai.personal@gmail.com>
2017-10-31 12:46:24 +0800

    syncModels.js change global sync to individual model sync, in order to get individual output of the sync status

--- src/server/controllers/database/syncModels.js
+++ src/server/controllers/database/syncModels.js
@@ -3,17 +3,26 @@ const Promise = require('bluebird')
 const eVars = require('../../config/eVars')
 const logging = require('../../controllers/logging')
 
-module.exports = (db) => {
+module.exports = (db, { force = null }) => {
   db.syncOps = []
   db.modelList.forEach((modelName) => {
-    db.syncOps.push(db[modelName].sync())
+    db.syncOps.push(
+      db[modelName]
+        .sync(force ? { force: true } : null)
+        .catch((error) => {
+          logging.error(error)
+          return Promise.reject(error)
+        })
+    )
   })
   return Promise
     .each(db.syncOps, (model) => {
-      if (eVars.ORM_VERBOSE) logging.console(`${model.name} 資料表同步... 完成`)
+      if (eVars.ORM_VERBOSE) {
+        logging.console(`${model.name} 資料表${force ? '強制刷新並' : ''}同步... 完成`)
+      }
     })
     .then(() => {
-      logging.console('資料庫同步... 完成')
+      logging.console(`資料庫${force ? '強制刷新並' : ''}同步... 完成`)
       return Promise.resolve()
     })
     .catch((error) => {
