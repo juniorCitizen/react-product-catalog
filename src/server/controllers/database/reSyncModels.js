e8dadbe757f1277f81d9d669cd6371d3f2a51592
chiayutsaipersonal <chiayu.tsai.personal@gmail.com>
2017-10-31 12:48:25 +0800

    set up choice to force refresh or regular sync through parameter setting

--- src/server/controllers/database/reSyncModels.js
+++ src/server/controllers/database/reSyncModels.js
@@ -1,10 +1,10 @@
 const logging = require('../../controllers/logging')
 
-module.exports = (db) => {
+module.exports = (db, { force = null }) => {
   return db.sequelize
-    .sync()
+    .sync(force ? { force: true } : null)
     .then(() => {
-      logging.console('資料庫重新同步... 完成')
+      logging.console(`資料庫${force ? '強制刷新並' : ''}重新同步... 完成`)
       return Promise.resolve()
     })
     .catch((error) => {
