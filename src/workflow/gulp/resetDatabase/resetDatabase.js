6a135b2b9a4de40ed314e4fb98af439825a56a3c
chiayutsaipersonal <chiayu.tsai.personal@gmail.com>
2017-11-02 00:23:26 +0800

    move gulp script file location

--- src/workflow/gulp/resetDatabase/resetDatabase.js
+++ src/workflow/gulp/resetDatabase/resetDatabase.js
@@ -1,18 +1,27 @@
 import { argv } from 'yargs'
 import Promise from 'bluebird'
 
-import db from '../../server/controllers/database/database'
-import logging from '../../server/controllers/logging'
+import db from '../../../server/controllers/database/database'
+import logging from '../../../server/controllers/logging'
 
 require('dotenv').config()
 
 const dbEnv = argv['setting'] || 'development'
-const seed = argv['seed'] || 'none'
+const seed = argv['seed'] || false
+
+// let countries = require('../mockData/countries')
+let labels = require('./labels')
+// let offices = require('../mockData/offices')
+let photos = require('./photos')
+let products = require('./products')
+let series = require('./series')
+let tags = require('./tags')
+// let users = require('../mockData/users')
 
 module.exports = () => {
   return (done) => {
     // validate arguments
-    if (!checkArgs(dbEnv, seed)) {
+    if (!checkArgs(dbEnv)) {
       let error = new Error('INVALID_ARGUMENTS')
       error.name = '參數錯誤'
       error.message = `獲取的取參數: dbEnv: ${dbEnv} | seed: ${seed}`
@@ -20,7 +29,7 @@ module.exports = () => {
       return done(error)
     }
     // get database configuration
-    let dbConfig = require('../../server/config/database')[dbEnv]
+    let dbConfig = require('../../../server/config/database')[dbEnv]
     // switch out the sequelize instance
     db.sequelize = new db.Sequelize(dbConfig)
     // start the reset process with disabling the database constraints
@@ -35,33 +44,57 @@ module.exports = () => {
         return enableConstraint(dbConfig.dialect)
       })
       .then(() => {
-        if (seed === 'none') {
-          // if cmd arg is empty or none, skip the data generation part the script
+        if (!seed) {
+          logging.warning('資料庫淨空完畢，未載入資料...')
           return done()
         } else {
-          // continue with the script
-          return Promise.resolve()
+          logging.warning('裝載基礎/預設資料...')
+          return series(db.Series)
+            .then(seriesIdList => products(db.Products, seriesIdList))
+            .then(() => tags(db.Tags))
+            .then(() => labels(db.Products, db.Tags))
+            .then(() => photos(db.Photos, db.Products))
+            .then(() => {
+              logging.warning('資料庫重設，並以完成預設資料載入...')
+              return done()
+            })
+            .catch((error) => {
+              logging.error(error, '預設資料載入失敗...')
+              return Promise.reject(error)
+            })
+          // // 建立產品系列資料
+          // return db.Series.bulkCreate(series())
+          //   .then(() => {
+          //     // 查詢產品系列資料筆數
+          //     return db.Series.findAndCount()
+          //   })
+          //   .then((seriesCount) => {
+          //     // 建立產品資料
+          //     return db.Products.bulkCreate(products(seriesCount.count))
+          //   })
+          //   .then(() => {
+          //     // 建立產品標題項目
+          //     return db.Tags.bulkCreate(tags())
+          //   })
+          //   .then(() => {
+          //     // 查詢產品以及產品標題項目資料
+          //     return Promise
+          //       .all([
+          //         db.Products.findAll(),
+          //         db.Tags.findAll()
+          //       ])
+          //   })
+          //   .spread(labels)
+          //   .then(() => {
+          //     return Promise.resolve()
+          //   })
+          //   .then(() => {
+          //     // TODO // 'default' data generation script
+          //     return Promise.resolve()
+          //   })
+          //   .then(() => {
+          //   })
         }
-      })
-      .then(() => {
-        // TODO // 'default' data generation script
-        return Promise.resolve()
-      })
-      .then(() => {
-        if (seed === 'default') {
-          // if cmd arg is 'default', skip the mock data generation
-          return done()
-        } else {
-          // continue with the script
-          return Promise.resolve()
-        }
-      })
-      .then(() => {
-        // TODO // 'mock' data generation script
-        return Promise.resolve()
-      })
-      .then(() => {
-        return done()
       })
       .catch((error) => {
         // in case of error, enable the database constraints first
@@ -110,7 +143,7 @@ function enableConstraint (dialect) {
       queryString = 'PRAGMA foreign_keys = ON;'
       break
     case 'mysql':
-      queryString = 'SET FOREIGN_KEY_CHECKS = OFF;'
+      queryString = 'SET FOREIGN_KEY_CHECKS = 1;'
       break
     default:
       return Promise.reject(new Error('INVALID_DB_DIALECT'))
@@ -128,11 +161,8 @@ function enableConstraint (dialect) {
 }
 
 // function to validate command line argument
-function checkArgs (dbEnv, seed) {
-  if (
-    ['development', 'staging', 'production'].indexOf(dbEnv) !== -1 &&
-    ['mock', 'mock', 'none'].indexOf(seed) !== -1
-  ) {
+function checkArgs (dbEnv) {
+  if (['development', 'staging', 'production'].indexOf(dbEnv) !== -1) {
     return true
   }
 }
