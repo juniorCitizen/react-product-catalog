c32e62ab5d02e38e3dc76f970d014973b5b483a2
chiayutsaipersonal <chiayu.tsai.personal@gmail.com>
2017-11-03 01:49:53 +0800

    json web token reworked jwt related code

--- src/server/middlewares/botPrevention.js
+++ src/server/middlewares/botPrevention.js
@@ -1,18 +1,16 @@
-import routerResponse from '../controllers/routerResponse'
+const logging = require('../controllers/logging')
+const routerResponse = require('../controllers/routerResponse')
 
 module.exports = (req, res, next) => {
   if (req.body.botPrevention === '') {
     next()
   } else {
-    let error = new Error('bot-like activities detected')
-    error.name = 'botPrevention'
+    logging.warning('bot-like activity detected')
     return routerResponse.json({
-      pendingResponse: res,
-      originalRequest: req,
+      req: req,
+      res: res,
       statusCode: 500,
-      success: false,
-      error: error,
-      message: error.message
+      message: ''
     })
   }
 }
