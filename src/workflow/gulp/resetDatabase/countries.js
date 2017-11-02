5b0d063e68193886599559a50f6903b73958de7f
chiayutsaipersonal <chiayu.tsai.personal@gmail.com>
2017-11-02 12:32:35 +0800

    moved file location

---
+++ src/workflow/gulp/resetDatabase/countries.js
@@ -0,0 +1,16 @@
+module.exports = () => {
+  return Promise.resolve()
+}
+
+// module.exports = (() => {
+//   let worldCountries = require('world-countries/dist/countries.json')
+//   let records = []
+//   worldCountries.forEach((worldCountry) => {
+//     records.push({
+//       id: worldCountry.cca3,
+//       name: worldCountry.name.common,
+//       region: worldCountry.region === '' ? 'Other' : worldCountry.region
+//     })
+//   })
+//   return records
+// })()
