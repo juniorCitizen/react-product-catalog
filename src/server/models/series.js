b78c56d15428a4af068d0148860c7306ddda0953
chiayutsaipersonal <chiayu.tsai.personal@gmail.com>
2017-11-03 01:50:31 +0800

    model series: updated 'name' field to being unique

--- src/server/models/series.js
+++ src/server/models/series.js
@@ -6,7 +6,8 @@ module.exports = (sequelize, DataTypes) => {
     },
     name: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
+      unique: true
     },
     displaySequence: {
       type: DataTypes.INTEGER,
