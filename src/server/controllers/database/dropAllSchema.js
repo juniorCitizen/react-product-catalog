3207230e64aef84f91d7027699a1ef0b5a11d3ca
chiayutsaipersonal <chiayu.tsai.personal@gmail.com>
2017-11-06 12:01:12 +0800

    move drop schema sequence to controller module

--- src/server/controllers/database/dropAllSchema.js
+++ src/server/controllers/database/dropAllSchema.js
@@ -2,20 +2,8 @@ const Promise = require('bluebird')
 
 const logging = require('../../controllers/logging')
 
-module.exports = (sequelize) => {
+module.exports = (sequelize, sequence) => {
   logging.warning('所有資料表將被依序移除')
-  let sequence = [
-    'users',
-    'offices',
-    'countries',
-    'interests',
-    'registrations',
-    'labels',
-    'tags',
-    'photos',
-    'products',
-    'series'
-  ]
   let actions = []
   sequence.forEach((tableName) => {
     actions.push(
