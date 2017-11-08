fed1045160e56a9746bf58585c96760069468dd2
chiayutsaipersonal <chiayu.tsai.personal@gmail.com>
2017-11-09 00:27:14 +0800

    fix path/endpoint mistake

--- src/server/middlewares/404Handler.js
+++ src/server/middlewares/404Handler.js
@@ -10,6 +10,6 @@ const eVars = require(path.join(accessPath, 'config/eVars'))
 const logging = require(path.join(accessPath, 'controllers/logging'))
 
 module.exports = (req, res) => {
-  logging.warning(`由客戶端接收到未配置的端點要求: ${eVars.HOST}${req.path}`)
-  return res.redirect(`${eVars.HOST}/${eVars.SYS_REF}`)
+  logging.warning(`由客戶端接收到未配置的端點要求: ${eVars.PROTOCOL}://${eVars.DOMAIN}:${eVars.PORT}${req.path}`)
+  return res.redirect(`${eVars.PROTOCOL}://${eVars.DOMAIN}:${eVars.PORT}/${eVars.SYS_REF}`)
 }
