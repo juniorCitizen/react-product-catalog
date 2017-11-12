54e406d0c33ad35198dfc8a8b7b47859bfae27b1
chiayutsaipersonal <chiayu.tsai.personal@gmail.com>
2017-11-13 01:14:39 +0800

    api routes delete product
    remove deprecated scripts

--- src/server/routes/products.js
+++ src/server/routes/products.js
@@ -4,6 +4,7 @@ const express = require('express')
 const getProducts = require('./products/getProducts')
 const insertProduct = require('./products/insertProduct')
 const updateProduct = require('./products/updateProduct')
+const deleteProduct = require('./products/deleteProduct')
 const notImplemented = require('../middlewares/notImplemented')
 
 // const upload = multer({ dest: path.join(__dirname, '../../upload/') })
@@ -13,74 +14,4 @@ module.exports = express.Router()
   .post('/', ...insertProduct)
   .put('/', ...updateProduct)
   .patch('/', notImplemented)
-  .delete('/', notImplemented)
-// .get('/series', require('./series'))
-// .get('/searchByCode', require('./products/searchByCode'))
-// .post('/',
-//   validateJwt,
-//   upload.fields([
-//     { name: 'primaryPhoto', maxCount: 1 },
-//     { name: 'secondaryPhotos', maxCount: 15 }
-//   ]),
-//   require('./products/insert'))
-// .put('/',
-//   validateJwt,
-//   upload.fields([
-//     { name: 'primaryPhoto', maxCount: 1 },
-//     { name: 'secondaryPhotos', maxCount: 15 }
-//   ]),
-//   require('./products/update'))
-// .delete('/', validateJwt, require('./products/delete'))
-
-// function products (req, res) {
-//   let queryFilter = {
-//     attributes: {
-//       exclude: ['createdAt', 'updatedAt', 'deletedAt']
-//     },
-//     include: [{
-//       model: db.Descriptions,
-//       attributes: {
-//         exclude: [
-//           'createdAt',
-//           'updatedAt',
-//           'deletedAt'
-//         ]
-//       }
-//     }, {
-//       model: db.Photos,
-//       attributes: {
-//         exclude: [
-//           'data',
-//           'createdAt',
-//           'updatedAt',
-//           'deletedAt'
-//         ]
-//       }
-//     }],
-//     order: [
-//       ['code'],
-//       [db.Photos, 'primary', 'DESC']
-//     ]
-//   }
-//   return db.Products.findAll(queryFilter)
-//     .then((productRecords) => {
-//       return routerResponse.json({
-//         pendingResponse: res,
-//         originalRequest: req,
-//         statusCode: 200,
-//         success: true,
-//         data: productRecords
-//       })
-//     })
-//     .catch((error) => {
-//       return routerResponse.json({
-//         pendingResponse: res,
-//         originalRequest: req,
-//         statusCode: 500,
-//         success: false,
-//         error: error.name,
-//         message: error.message,
-//         data: error.stack
-//       })
-//     })
-// }
+  .delete('/', ...deleteProduct)
