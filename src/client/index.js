c0f92e5cfea49034b8f9c05c41ab9a26be0479e6
林于斌 <papago75@gmail.com.tw>
2017-11-14 20:16:49 +0800

    fix bug

---
+++ src/client/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Main from './components/main'
+import { Provider } from 'react-redux'
+import configureStore from './store/configureStore'
+const store = configureStore();
+
+ReactDOM.render((
+    <Provider store={store}>
+        <Main />
+    </Provider>
+), app) 
