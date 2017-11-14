35629896470d4d20868ac7e0f018ea23156390c8
chiayutsaipersonal <chiayu.tsai.personal@gmail.com>
2017-11-14 20:28:09 +0800

    clean up code in middlewares

--- src/server/middlewares/validatePasswordFormat.js
+++ src/server/middlewares/validatePasswordFormat.js
@@ -1,16 +1,16 @@
 const routerResponse = require('../controllers/routerResponse')
 
 module.exports = (req, res, next) => {
-  if (
-    (!req.body.password) ||
-    ((req.body.password.length < 8) || (req.body.password.length > 20))
-  ) {
-    return routerResponse.json({
-      req: req,
-      res: res,
-      statusCode: 401,
-      message: 'password not transmitted or is of incorrect length'
+  if (!req.body.hasOwnProperty('password')) {
+    routerResponse.json({
+      req, res, statusCode: 401, message: 'password not found'
     })
+    return next('PASSWORD_NOT_FOUND')
+  } else if ((req.body.password.length < 8) || (req.body.password.length > 20)) {
+    routerResponse.json({
+      req, res, statusCode: 401, message: 'wrong password format'
+    })
+    return next('WRONG_PASSWORD_FORMAT')
   }
   next()
 }
