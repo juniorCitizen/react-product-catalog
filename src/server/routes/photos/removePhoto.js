4eb0a9f034ef09bbe93bdadd1d493a4cf8f185fd
chiayutsaipersonal <chiayu.tsai.personal@gmail.com>
2017-11-19 22:58:26 +0800

    rework API endpoints 1. to use proper error handling 2. use middleware to process responses

--- src/server/routes/photos/removePhoto.js
+++ src/server/routes/photos/removePhoto.js
@@ -1,19 +1,18 @@
 const db = require('../../controllers/database')
-const routerResponse = require('../../controllers/routerResponse')
 
 const validateJwt = require('../../middlewares/validateJwt')
 
 module.exports = (() => {
   return [
     validateJwt,
-    (req, res) => {
+    (req, res, next) => {
       return db.Photos
         .destroy({ where: { id: req.params.photoId.toUpperCase() } })
-        .then((data) => routerResponse.json({
-          req, res, statusCode: 200, data
-        }))
-        .catch(error => routerResponse.json({
-          req, res, statusCode: 500, error
-        }))
+        .then(data => {
+          req.resJson = { data }
+          next()
+          return Promise.resolve()
+        })
+        .catch(error => next(error))
     }]
 })()
