bc075871de407ea30ea0a7ab9a580d5c377d8edc
chiayutsaipersonal <chiayu.tsai.personal@gmail.com>
2017-11-28 00:18:38 +0800

    correct response status code

--- src/server/middlewares/validatePasswordFormat.js
+++ src/server/middlewares/validatePasswordFormat.js
@@ -1,11 +1,11 @@
 module.exports = (req, res, next) => {
   if (!('password' in req.body)) {
-    res.status(401)
+    res.status(400)
     let error = new Error('Password not found')
     return next(error)
   } else if ((req.body.password.length < 8) || (req.body.password.length > 20)) {
-    res.status(401)
-    let error = new Error('Incorrect password format')
+    res.status(400)
+    let error = new Error('Illegal password length')
     error.message = 'Password should be 8 to 20 characters long'
     return next(error)
   }
