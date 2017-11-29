a094a58b73e3f1fdc02e2227b807043518c6537b
chiayutsaipersonal <chiayu.tsai.personal@gmail.com>
2017-11-29 15:07:09 +0800

    code update

--- src/server/routes/photos/removePhotoById.js
+++ src/server/routes/photos/removePhotoById.js
@@ -2,17 +2,16 @@ const db = require('../../controllers/database')
 
 const validateJwt = require('../../middlewares/validateJwt')
 
-module.exports = (() => {
-  return [
-    validateJwt({ admin: true }),
-    (req, res, next) => {
-      return db.Photos
-        .destroy({ where: { id: req.params.photoId.toUpperCase() } })
-        .then(data => {
-          req.resJson = { data }
-          next()
-          return Promise.resolve()
-        })
-        .catch(error => next(error))
-    }]
-})()
+module.exports = [
+  validateJwt({ admin: true }),
+  (req, res, next) => {
+    return db.Photos
+      .destroy({ where: { id: req.params.photoId.toUpperCase() } })
+      .then(data => {
+        req.resJson = { data }
+        next()
+        return Promise.resolve()
+      })
+      .catch(error => next(error))
+  }
+]
