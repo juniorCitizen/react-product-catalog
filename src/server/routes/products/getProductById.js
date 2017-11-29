07b40af9c74131f710c7f1d9df6b8dd2968b3c0c
chiayutsaipersonal <chiayu.tsai.personal@gmail.com>
2017-11-29 23:06:02 +0800

    product API get product information by id working

--- src/server/routes/products/getProductById.js
+++ src/server/routes/products/getProductById.js
@@ -1,22 +1,24 @@
 const db = require('../../controllers/database')
 
-const modelReference = 'products'
-
-const setBaseQueryParameters = require('../../middlewares/setQueryBaseOptions')(modelReference)
-const setResponseDetailLevel = require('../../middlewares/setResponseDetailLevel')(modelReference)
-
-module.exports = (() => {
-  return [
-    setBaseQueryParameters,
-    setResponseDetailLevel,
-    (req, res, next) => {
-      return db.Products
-        .findById(req.params.productId.toUpperCase(), req.queryOptions)
-        .then(data => {
-          req.resJson = { data }
-          next()
-          return Promise.resolve()
-        })
-        .catch(error => next(error))
-    }]
-})()
+module.exports = [
+  (req, res, next) => {
+    return db.Products
+      .findById(req.params.productId.toUpperCase(), {
+        include: [
+          { model: db.Tags },
+          { model: db.Photos, attributes: { exclude: ['data'] } }
+        ],
+        order: [
+          'code',
+          [db.Tags, 'name'],
+          [db.Photos, 'primary', 'desc']
+        ]
+      })
+      .then(data => {
+        req.resJson = { data }
+        next()
+        return Promise.resolve()
+      })
+      .catch(error => next(error))
+  }
+]
