6deab57fab2e59be72b56fdca8686bf04858a069
林于斌 <papago75@gmail.com.tw>
2017-12-02 20:03:47 +0800

    pull

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -10,7 +10,6 @@ module.exports = express.Router()
         view: 'index',
         data: {
           title: eVars.SYS_REF,
-<<<<<<< HEAD
           faviconSource: `${eVars.APP_ROUTE}/dist/public/favicon.ico`,
           scriptSource: !eVars.devMode
             ? `${eVars.APP_ROUTE}/dist/public/app.min.js`
@@ -21,18 +20,6 @@ module.exports = express.Router()
           styleSource: !eVars.devMode
             ? `${eVars.APP_ROUTE}/dist/public/app-bluma.min.css`
             : `${eVars.APP_ROUTE}/dist/public/app-bluma.css`,
-=======
-          faviconSource: `${eVars.APP_ROUTE}/favicon.ico`,
-          scriptSource: !eVars.devMode
-            ? `${eVars.APP_ROUTE}/app.min.js`
-            : `${eVars.APP_ROUTE}/app.js`,
-          vendorSource: !eVars.devMode
-            ? `${eVars.APP_ROUTE}/vendor.min.js`
-            : `${eVars.APP_ROUTE}/vendor.js`,
-          styleSource: !eVars.devMode
-            ? `${eVars.APP_ROUTE}/app.min.css`
-            : `${eVars.APP_ROUTE}/app-bluma.css`,
->>>>>>> a96c8dce52180fc2a72bce43a0b538f75f3a373c
           appUrl: eVars.APP_ROUTE
         }
       }
